7047da2865fa2f5c918b1d15dd6c092b8bcc541e
7kaji <hirotonbuu@gmail.com>
2020-06-13 22:51:55 +0900

    fixed initial query

--- data.sql
+++ data.sql
@@ -1,10 +1,9 @@
-CREATE TABLE `books`(
-   `id` bigint unsigned(11) NOT NULL AUTO_INCREMENT,
-   `title` varchar(100) NOT NULL,
-   `description` varchar(150) NOT NULL,
-   `price` varchar(200) NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE=utf8mb4_bin;
-
+Create Table: CREATE TABLE `books` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(150) COLLATE utf8mb4_bin NOT NULL,
+  `price` varchar(200) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
